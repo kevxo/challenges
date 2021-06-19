8809122cc526be92564ac3c09612f9dc7050f672
Kevin Cuadros <kev040798@gmail.com>
2021-06-18 20:42:49 -0400

    Finish challenge

--- lib/password_checker.rb
+++ lib/password_checker.rb
@@ -1,5 +1,26 @@
 class PasswordChecker
+  def initialize(pw_hash)
+    @hash = JSON.parse(pw_hash)
+  end
 
+  def count_valid
+    count = 0
 
+    @hash.each do |key, value|
+      split_key = key.split(' ')
+      get_range = split_key.first.split('-')
+      range = (get_range[0].to_i..get_range[1].to_i).to_a
+      letter = split_key.last
+      value_array = value.split('')
+      letter_count = 0
 
+      value_array.each do |ele|
+        letter_count += 1 if ele == letter
+      end
+
+      count += 1 if range.include?(letter_count)
+    end
+
+    count
+  end
 end
