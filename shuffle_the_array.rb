bd37cc7e67089c664ffed1b6f08d57410f9f9123
Kevin Cuadros <kev040798@gmail.com>
2021-07-24 00:00:41 -0400

    Finish shuffle array for both python and ruby

--- shuffle_the_array.rb
+++ shuffle_the_array.rb
@@ -4,15 +4,12 @@ n = 3
 def shuffle(nums, n)
   new_arr = []
 
-  (nums.length - 1).times do |i|
-    x = nums[i]
-    y = nums[i + n] unless (nums[i + n]).nil?
-    pair = [x, y] unless [x, y].include?(nil)
-
-    new_arr.push(pair) unless pair.nil?
+  n.times do |i|
+    new_arr.push(nums[i])
+    new_arr.push(nums[i + n])
   end
 
-  new_arr.flatten
+  new_arr
 end
 
 p shuffle(nums, n)
