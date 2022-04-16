94b80a00141522ec7e74a23cb9cdc774bc9657eb
Kevin Cuadros <kev040798@gmail.com>
2022-04-15 21:20:09 -0400

    Finish is isogram challenge

---
+++ python/is_isogram.py
@@ -0,0 +1,31 @@
+string = "Dermatoglyphics"
+string2 = "aba"
+string3 = "moOse"
+
+def is_isogram(string):
+  list_string = list(string.lower())
+
+  for char, count in char_dict(list_string).items():
+    if count > 1:
+      return False
+
+  return True
+
+
+def char_dict(list_string):
+  char_dict = {}
+
+  for char in list_string:
+    if char in char_dict:
+      char_dict[char] += 1
+    else:
+      char_dict[char] = 1
+
+  return char_dict
+
+
+
+print(is_isogram(string))
+print(is_isogram(string2))
+print(is_isogram(string3))
+
