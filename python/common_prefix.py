8cba21ef4f042454249cbaa31c28cd18471595b4
Kevin Cuadros <kev040798@gmail.com>
2022-07-07 19:18:37 -0400

    WIP

---
+++ python/common_prefix.py
@@ -0,0 +1,13 @@
+from typing import List
+
+strings: List[str] = ["flower","flow","flight"]
+
+def common_prefix(strings: List[str]) -> str:
+  count = 0
+  answer = ''
+
+  for string in strings:
+    pass
+
+
+print(common_prefix(strings))
