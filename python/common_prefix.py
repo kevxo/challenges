ef6c55fa91b4f01f1004b6b7de5f900ce1c4864e
Kevin Cuadros <kev040798@gmail.com>
2022-07-08 17:10:57 -0400

    Finish common prefix challenge

--- python/common_prefix.py
+++ python/common_prefix.py
@@ -3,11 +3,21 @@ from typing import List
 strings: List[str] = ["flower","flow","flight"]
 
 def common_prefix(strings: List[str]) -> str:
-  count = 0
   answer = ''
+  prefix = strings[0]
 
-  for string in strings:
-    pass
+  for string in strings[1:]:
+    while string[:len(prefix)] != prefix and prefix:
+      prefix = prefix[:len(prefix) - 1]
+
+      if not prefix:
+        break
+
+  answer = prefix
+
+  return answer
+
+
 
 
 print(common_prefix(strings))
